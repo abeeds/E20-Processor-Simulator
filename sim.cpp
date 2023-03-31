016e5353450f256c3972c95f098633c8340f0e91
Shahria Abeed <02sabeed@gmail.com>
2023-03-31 19:48:04 -0400

    merged to sim.cpp

--- sim.cpp
+++ sim.cpp
@@ -14,27 +14,9 @@ size_t const static NUM_REGS = 8;
 size_t const static MEM_SIZE = 1<<13;
 size_t const static REG_SIZE = 1<<16;
 
-/*
-    Loads an E20 machine code file into the list
-    provided by mem. We assume that mem is
-    large enough to hold the values in the machine
-    code file.
 
-    @param f Open file to read from
-    @param mem Array representing memory into which to read program
-*/
-void load_machine_code(ifstream &f, unsigned mem[]);
+size_t load_machine_code(ifstream &f, uint16_t mem[]);
 
-/*
-    Prints the current state of the simulator, including
-    the current program counter, the current register values,
-    and the first memquantity elements of memory.
-
-    @param pc The final value of the program counter
-    @param regs Final value of all registers
-    @param memory Final value of memory
-    @param memquantity How many words of memory to dump
-*/
 // 3 reg args ---------------------------------------------
 void func_add(uint16_t &dst, uint16_t srca, uint16_t srcb);
 
@@ -47,7 +29,7 @@ void func_and(uint16_t &dst, uint16_t srca, uint16_t srcb);
 void func_slt(uint16_t &dst, uint16_t srca, uint16_t srcb);
 
 
-// 2 reg args 
+// 2 reg args ---------------------------------------------
 
 void func_slti(uint16_t &dst, uint16_t src, uint16_t imm);
 
@@ -56,7 +38,13 @@ uint16_t func_jeq(uint16_t srca, uint16_t srcb, uint16_t imm);
 void func_addi(uint16_t &dst, uint16_t src, uint16_t imm);
 
 
+// reads each instruction and executes the correct command
+void parse_instruc(uint16_t mem[], uint16_t registers[], uint16_t &progC);
 
+// used to determine if program should keep running
+bool is_halt(uint16_t mem[], uint16_t regs[], uint16_t pc);
+
+uint16_t sign_extend(uint16_t num);
 
 int main(int argc, char *argv[]) {
     /*
@@ -97,28 +85,36 @@ int main(int argc, char *argv[]) {
     }
 
     // -------------------------------------------------
-    //               Variable Assignments
-
-    // registers
-    uint16_t regs[8];
-    regs[0] = 0;
-
-    // memory
-    unsigned memory[8192];
-
-    // program counter
+    //              Variable Assignments
+    uint16_t regs[8] = {0};
+    uint16_t memory[8192];
     uint16_t pc = 0;
+    bool keepGoing = true;
+    size_t quantity;
     
+
     // -------------------------------------------------
     //          Store Instructions in Memory
 
-    load_machine_code(file, memory);
-    
-    // -------------------------------------------------
-    // TODO: your code here. Do simulation.
+    // store quantity for print
+    quantity = load_machine_code(file, memory);
 
+
+    // -------------------------------------------------
+    //                   Simulation
+
+    while(keepGoing){
+        keepGoing = !is_halt(memory, regs, pc);
+        parse_instruc(memory, regs, pc);
+
+        // ignore first 3 bits
+        pc = pc << 3;
+        pc = pc >> 3;
+    }
+
+    // -------------------------------------------------
     // TODO: your code here. print the final state of the simulator before ending, using print_state
-    
+    print_state(pc, regs, memory, quantity);
     return 0;
 }
 // 
@@ -126,8 +122,7 @@ int main(int argc, char *argv[]) {
 // All prototype implementations below:
 
 
-
-void load_machine_code(ifstream &f, unsigned mem[]) {
+size_t load_machine_code(ifstream &f, uint16_t mem[]) {
     regex machine_code_re("^ram\\[(\\d+)\\] = 16'b(\\d+);.*$");
     size_t expectedaddr = 0;
     string line;
@@ -142,7 +137,7 @@ void load_machine_code(ifstream &f, unsigned mem[]) {
 
         // ensures instructions increment properly
         size_t addr = stoi(sm[1], nullptr, 10);
-        unsigned instr = stoi(sm[2], nullptr, 2);
+        uint16_t instr = stoi(sm[2], nullptr, 2);
         if (addr != expectedaddr) {
             cerr << "Memory addresses encountered out of sequence: " << addr << endl;
             exit(1);
@@ -156,12 +151,259 @@ void load_machine_code(ifstream &f, unsigned mem[]) {
         expectedaddr ++;
         mem[addr] = instr; // store instruction in array
     }
+    return expectedaddr;
+}
+
+void print_state(uint16_t pc, uint16_t regs[], uint16_t memory[], size_t memquantity) {
+    cout << setfill(' ');
+    cout << "Final state:" << endl;
+    cout << "\tpc=" <<setw(5)<< pc << endl;
+
+    for (size_t reg=0; reg<NUM_REGS; reg++)
+        cout << "\t$" << reg << "="<<setw(5)<<regs[reg]<<endl;
+
+    cout << setfill('0');
+    bool cr = false;
+    for (size_t count=0; count<memquantity; count++) {
+        cout << hex << setw(4) << memory[count] << " ";
+        cr = true;
+        if (count % 8 == 7) {
+            cout << endl;
+            cr = false;
+        }
+    }
+    if (cr)
+        cout << endl;
 }
 
 
+void parse_instruc(uint16_t mem[], uint16_t registers[], uint16_t &progC){
+    uint16_t instruc = mem[progC];
+    uint16_t opcode = instruc >> 13;
+    
+    // isolate registers
+    uint16_t reg1 = instruc;
+    uint16_t reg2 = instruc;
+    uint16_t reg3 = instruc;
+    
+    reg1 = reg1 << 3;
+    reg1 = reg1 >> 13;
+
+    reg2 = reg2 << 6;
+    reg2 = reg2 >> 13;
+
+    reg3 = reg3 << 9;
+    reg3 = reg3 >> 13;
 
 
-void add(uint16_t &dst, uint16_t srca, uint16_t srcb) {
+
+
+    // 3 reg argument
+    if(opcode == 0) {
+        // isolate function code
+        uint16_t func_code = instruc;
+        func_code = func_code << 12;
+        func_code = func_code >> 12;
+
+        // the functions inside this should not execute
+        // if reg3/regDST is 0, $0 should remain 0 at all times
+        if(reg3 != 0) {
+            // add (3, 1, 2)
+            if(func_code == 0) {
+                func_add(registers[reg3], registers[reg1], registers[reg2]);
+                progC += 1;
+                return;
+            }
+
+            // sub
+            if(func_code == 1) {
+                func_sub(registers[reg3], registers[reg1], registers[reg2]);
+                progC += 1;
+                return;
+            }
+
+            // or 
+            if (func_code == 2) {
+                func_or(registers[reg3], registers[reg1], registers[reg2]);
+                progC += 1;
+                return;
+            }
+
+            // and
+            if (func_code == 3) {
+                func_and(registers[reg3], registers[reg1], registers[reg2]);
+                progC += 1;
+                return;
+            }
+
+            // slt
+            if (func_code == 4) {
+                func_slt(registers[reg3], registers[reg1], registers[reg2]);
+                progC += 1;
+                return;
+            }
+        }
+
+        // jr
+        if (func_code == 8) {
+            progC = registers[reg1];
+            return;
+        }
+
+
+    }
+
+    // 2 reg args
+
+    // isolate immediate (7-bit)
+    uint16_t imm_7 = instruc;
+    imm_7 = imm_7 << 9;
+    imm_7 = imm_7 >> 9;
+
+    // slti
+    if (opcode == 7) {
+        imm_7 = sign_extend(imm_7);
+        func_slti(registers[reg2], registers[reg1], imm_7);
+        progC += 1;
+        return;
+    }
+
+    // lw
+    if (opcode == 4) {
+        progC += 1;
+
+        // do nothing if regDST is 0
+        if(reg2 == 0) {
+            return;
+        }
+
+        imm_7 = sign_extend(imm_7);
+
+        registers[reg2] = mem[imm_7 + registers[reg1]];
+        return;
+    }
+
+    // sw
+    if (opcode == 5) {
+        imm_7 = sign_extend(imm_7);
+
+        mem[imm_7 + registers[reg1]] = registers[reg2];
+
+        progC += 1;
+    }
+
+    // jeq
+    if (opcode == 6) {
+        if(registers[reg1] == registers[reg2]) {
+            imm_7 = sign_extend(imm_7);
+            progC += 1 + imm_7;
+            return;
+        }
+        progC += 1;
+        return;
+    }
+
+    // addi 
+    if (opcode == 1) {
+        progC += 1;
+
+        // do nothing if regDST is 0
+        if(reg2 == 0){
+            return;
+        }
+
+        imm_7 = sign_extend(imm_7);
+        func_addi(registers[reg2], registers[reg1], imm_7);
+        return;
+    }
+
+    // no reg args
+
+    // isolate immediate (13-bit)
+    uint16_t imm_13 = instruc;
+    imm_13 = imm_13 << 3;
+    imm_13 = imm_13 >> 3;
+
+    // j
+    if (opcode == 2) {
+        progC = imm_13;
+        return;
+    }
+
+    // jal
+    // 0101111000000000
+    if (opcode == 3) {
+        registers[7] = progC + 1;
+        progC = imm_13;
+        return;
+    }
+} // parse_instruc
+
+
+
+uint16_t sign_extend(uint16_t num) {
+    uint16_t msb = num << 9;
+    msb = msb >> 15;
+    uint16_t temp = 0b1111111110000000;
+    uint16_t out;
+
+    if(msb == 0){
+        return num;
+    }
+    return (num | temp);
+}
+
+bool is_halt(uint16_t mem[], uint16_t regs[], uint16_t pc) {
+    uint16_t instruction = mem[pc];
+
+    uint16_t opcode = instruction;
+    opcode = opcode >> 13;
+    
+    uint16_t func_code = instruction;
+    func_code = func_code << 12;
+    func_code = func_code >> 12;
+
+
+    // halt by jr
+    if(opcode == 0 && func_code == 8) {
+        uint16_t regSRC = instruction;
+        regSRC = regSRC << 3;
+        regSRC = regSRC >> 13;
+
+        return regs[regSRC] == pc; 
+    }
+
+
+    // halt by jeq
+    if(opcode == 6) {
+        uint16_t reg1, reg2, imm;
+        reg1 = instruction << 3;
+        reg1 = reg1 >> 13;
+
+        reg2 = instruction << 6;
+        reg2 = reg2 >> 13;
+
+        imm = instruction << 9;
+        imm = imm >> 9;
+
+
+        return regs[reg1] == regs[reg2] && imm == 0;
+    }
+
+
+    // halt by j or jal
+    if(opcode == 2 || opcode == 3) {
+        uint16_t imm = instruction;
+        imm = imm << 3;
+        imm = imm >> 3;
+
+        return imm == pc;
+    }
+
+    return false; // any remaining cases;
+}
+
+void func_add(uint16_t &dst, uint16_t srca, uint16_t srcb) {
     dst = srca + srcb;
 }
 
@@ -170,7 +412,7 @@ void func_sub(uint16_t &dst, uint16_t srca, uint16_t srcb){
 }
 
 void func_or(uint16_t &dst, uint16_t srca, uint16_t srcb){
-    dst = srca ^ srcb;
+    dst = srca | srcb;
 }
 
 void func_and(uint16_t &dst, uint16_t srca, uint16_t srcb) {
@@ -196,4 +438,7 @@ void func_slti(uint16_t &dst, uint16_t src, uint16_t imm) {
 void func_addi(uint16_t &dst, uint16_t src, uint16_t imm) {
     dst = src + imm;
 }
+
+
+
 
